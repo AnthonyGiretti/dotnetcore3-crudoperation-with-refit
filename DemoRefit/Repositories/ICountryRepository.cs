56c2caa141c8b14189717bbf08c7568e9e712d14
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-03 18:20:54 -0500

    add service contracts

---
+++ DemoRefit/Repositories/ICountryRepository.cs
@@ -0,0 +1,30 @@
+﻿using DemoRefit.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoRefit.Repositories
+{
+    public interface ICountryRepository
+    {
+        [Get("/api/democrud")]
+        Task<IEnumerable<Country>> GetAsync();
+
+        [Get("/api/democrud/{id}")]
+        Task<Country> GetAsync(int id);
+
+        [Post("/api/democrud/create")]
+        Task<Country> PostAsync([Body] Country country);
+
+        [Put("/api/democrud/update/{id}")]
+        Task<Country> PutAsync(int id, [Body] Country country);
+
+        [Patch("/api/democrud/update/{id}/description")]
+        Task<Country> PatchAsync(int id, [Body]string description);
+
+        [Delete("/api/democrud/delete/{id}")]
+        Task<Country> DeleteAsync(int id);
+    }
+}
