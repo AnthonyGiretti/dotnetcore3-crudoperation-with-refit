eb8ab22d7225f19ce059051b1126974dcbb6fa09
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-03 17:06:58 -0500

    initial commit

---
+++ RefitDemo/RefitDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
