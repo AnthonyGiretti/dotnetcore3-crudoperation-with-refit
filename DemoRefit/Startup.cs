7e95fc56f110f0c94c22d90f54dab33c9686dd8a
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-03 18:26:43 -0500

    add refit configuration for ICountryRepository

--- DemoRefit/Startup.cs
+++ DemoRefit/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DemoRefit.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Refit;
 
 namespace DemoRefit
 {
@@ -26,6 +28,9 @@ namespace DemoRefit
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddRefitClient<ICountryRepository>()
+                    .ConfigureHttpClient(c => c.BaseAddress = new Uri(Configuration.GetSection("Apis:ApiCountry:Url").Value));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
