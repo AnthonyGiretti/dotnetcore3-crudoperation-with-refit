2bd559e732c97bb848d5bfec9d7129abe0b96c05
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-30 22:32:45 -0500

    remove useless using

--- DemoRefit/HttpClients/CountryRepositoryClientV2.cs
+++ DemoRefit/HttpClients/CountryRepositoryClientV2.cs
@@ -3,7 +3,6 @@ using DemoRefit.Repositories;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
