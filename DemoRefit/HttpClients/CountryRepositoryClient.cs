9d59e0421803a442901a0018f6f306114332cb03
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-30 21:15:33 -0500

    create new httpclient

---
+++ DemoRefit/HttpClients/CountryRepositoryClient.cs
@@ -0,0 +1,50 @@
+﻿using DemoRefit.Models;
+using DemoRefit.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DemoRefit.HttpClients
+{
+    public class CountryRepositoryClient : ICountryRepositoryClient
+    {
+        private readonly HttpClient _client;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ILogger<CountryRepositoryClient> _logger;
+
+
+        public CountryRepositoryClient(HttpClient client, ILogger<CountryRepositoryClient> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _client = client;
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<IEnumerable<Country>> GetAsync()
+        {
+            try
+            {
+                string tokenAsync = await _httpContextAccessor.HttpContext.GetTokenAsync("access_token");
+                if (string.IsNullOrEmpty(tokenAsync))
+                {
+                    throw new Exception("Access token is missing");
+                }
+
+                using (HttpResponseMessage response = await _client.GetAsync("/api/democrud"))
+                {
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsAsync<IEnumerable<Country>>();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to run http query");
+                return null;
+            }
+        }
+    }
+}
---
+++ DemoRefit/Repositories/ICountryRepositoryClient.cs
@@ -0,0 +1,13 @@
+﻿using DemoRefit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoRefit.Repositories
+{
+    public interface ICountryRepositoryClient
+    {
+        Task<IEnumerable<Country>> GetAsync();
+    }
+}
