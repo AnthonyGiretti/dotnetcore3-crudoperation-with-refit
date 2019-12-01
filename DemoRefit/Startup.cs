89e664e46b022759fd6e295060cfced9b75193b6
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-30 20:09:54 -0500

    add AddHttpContextAccessor

--- DemoRefit/Startup.cs
+++ DemoRefit/Startup.cs
@@ -21,6 +21,7 @@ namespace DemoRefit
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
             services.AddControllers();
             services.AddRefitClient<ICountryRepository>()
                     .ConfigureHttpClient(c => c.BaseAddress = new Uri(Configuration.GetSection("Apis:CountryApi:Url").Value));
