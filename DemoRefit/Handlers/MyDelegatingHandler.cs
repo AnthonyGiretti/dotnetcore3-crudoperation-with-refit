6bbb532fe06978df5b439370daa32c8584696430
Anthony Giretti <anthony.giretti@gmail.com>
2019-11-30 21:55:18 -0500

    clean using statements

--- DemoRefit/Handlers/MyDelegatingHandler.cs
+++ DemoRefit/Handlers/MyDelegatingHandler.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
